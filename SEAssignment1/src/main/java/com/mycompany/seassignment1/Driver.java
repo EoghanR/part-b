0946e01b111bcb35cde33ca92e54c3ef99a4279a
Eoghan <eoghan2297@gmail.com>
2018-10-18 00:32:23 +0100

    Close to done

---
+++ SEAssignment1/src/main/java/com/mycompany/seassignment1/Driver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.seassignment1;
+
+import org.joda.time.DateTime;
+
+/**
+ *
+ * @author Eoghan
+ */
+public class Driver {
+    public static void Main(String[] args){
+        Student student1 = new Student("Eoghan", 21, new DateTime(1997, 6, 22, 0, 0), 15310661);
+    }
+}
