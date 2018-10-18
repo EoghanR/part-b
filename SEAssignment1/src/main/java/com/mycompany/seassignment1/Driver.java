2e81177d5580deda2a51e40fb7e2b69ecfa10c42
Eoghan <eoghan2297@gmail.com>
2018-10-18 01:29:24 +0100

    Values for driver test set

--- SEAssignment1/src/main/java/com/mycompany/seassignment1/Driver.java
+++ SEAssignment1/src/main/java/com/mycompany/seassignment1/Driver.java
@@ -1,6 +1,7 @@
 package com.mycompany.seassignment1;
 
 import org.joda.time.DateTime;
+import java.util.ArrayList;
 
 /**
  *
@@ -8,12 +9,32 @@ import org.joda.time.DateTime;
  */
 public class Driver {
     public static void main(String[] args){
-        Student student1 = new Student("Eoghan", 21, new DateTime(1997, 6, 22, 0, 0), 15310661);
-        Student student2 = new Student("Neil", 23, new DateTime(1994, 11, 21, 0, 0), 12435233);
-        Student student3 = new Student("Colm", 26, new DateTime(1991, 11, 12, 0, 0), 10798534);
+        ArrayList<Student> students = new ArrayList<Student>();
+        ArrayList<Module> modules = new ArrayList<Module>();
+        ArrayList<Course> courses = new ArrayList<Course>();
         
-        System.out.println(student1.getUsername());
-        System.out.println(student2.getUsername());
-        System.out.println(student3.getUsername());
+        students.add(new Student("Eoghan", 21, new DateTime(1997, 6, 22, 0, 0), 15310661));
+        students.add(new Student("Neil", 23, new DateTime(1994, 11, 21, 0, 0), 12435233));
+        students.add(new Student("Colm", 26, new DateTime(1991, 11, 12, 0, 0), 10998534));
+        students.add(new Student("David", 22, new DateTime(1996, 10, 10, 0, 0), 15239856));
+        students.add(new Student("Eamonn", 24, new DateTime(1994, 8, 31, 0, 0), 12967435));
+        
+        modules.add(new Module("Programming", "CT417"));
+        modules.add(new Module("Telecomms", "EE453"));
+        modules.add(new Module("Electromagnetics", "EE348"));
+        modules.add(new Module("English", "AL443"));
+        modules.add(new Module("Film Studies", "AL475"));
+        modules.add(new Module("Economics", "BA421"));
+        modules.add(new Module("Physics", "SC456"));
+        modules.add(new Module("Business", "BA453"));
+        modules.add(new Module("Mathematics", "MA451"));
+        modules.add(new Module("Chemistry", "SC435"));
+        
+        courses.add(new Course("Computer Science", new DateTime(2018, 9, 10, 0, 0), new DateTime(2019, 5, 10, 0, 0)));
+        courses.add(new Course("Engineering", new DateTime(2018, 9, 10, 0, 0), new DateTime(2019, 5, 10, 0, 0)));
+        courses.add(new Course("Film Studies", new DateTime(2018, 9, 10, 0, 0), new DateTime(2019, 5, 10, 0, 0)));
+        courses.add(new Course("Business Studies", new DateTime(2018, 9, 10, 0, 0), new DateTime(2019, 5, 10, 0, 0)));
+        courses.add(new Course("General Science", new DateTime(2018, 9, 10, 0, 0), new DateTime(2019, 5, 10, 0, 0)));
+                
     }
 }
